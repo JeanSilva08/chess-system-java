f00e2e93a8826f34833002a8235e8c1f256f9fbc
Jean$ <jeancarlossoares.s@gmail.com>
2021-12-08 23:10:26 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if (!board.theresIsAPiece(position)) {
 			throw new ChessException("Não existe peça na posição de origem! ");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Não existe movimento possível para esta peça! ");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
